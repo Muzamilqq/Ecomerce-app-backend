1d46e1dceac4df0878e13e6bd484816945bee822
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-24 03:27:34 +0500

    name3

--- database/db.js
+++ database/db.js
@@ -1,24 +1,12 @@
-import pkg from "pg";
-const { Client } = pkg;
+import { Client } from "pg";
 import dotenv from "dotenv";
-
 dotenv.config({ path: "./config/config.env" });
 
 const database = new Client({
-  user: process.env.DB_USER, // e.g., "postgres"
-  host: process.env.DB_HOST, // e.g., "db.cduapyvabihbfjtdzfln.supabase.co"
-  database: process.env.DB_NAME, // e.g., "postgres"
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-  ssl: { rejectUnauthorized: false }, // required for Supabase
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
-try {
-  await database.connect();
-  console.log("Connected to the remote database successfully!");
-} catch (error) {
-  console.error("Database connection failed:", error);
-  process.exit(1);
-}
-
+await database.connect();
+console.log("Connected!");
 export default database;
