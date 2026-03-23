85aa50772bb4876b5a875cf568a09d3e218f5a98
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-24 02:29:09 +0500

    lag fix

---
+++ database/db.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
+import { Client } from "pg"; // PostgreSQL client
+
+// Debug to make sure env is loading
+console.log("DB_PASSWORD =", process.env.DB_PASSWORD);
+const database = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5433,
+});
+
+try {
+  await database.connect();
+  console.log("Connected to the database successfully");
+} catch (error) {
+  console.error("Database connection failed:", error);
+  process.exit(1);
+}
+
+export default database;
