ef79cde833d230b09ec375742eb3a7c4c4a0c843
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-24 02:53:13 +0500

    name

--- database/db.js
+++ database/db.js
@@ -1,18 +1,20 @@
+import { Client } from "pg";
 import dotenv from "dotenv";
-dotenv.config({ path: "./config/config.env" });
-import { Client } from "pg"; // PostgreSQL client
+
+dotenv.config();
 
 const database = new Client({
-  user: process.env.USER,
-  host: process.env.HOST,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
+  ssl: { rejectUnauthorized: false },
 });
 
 try {
   await database.connect();
-  console.log("Connected to the database successfully");
+  console.log("Connected to the remote database successfully!");
 } catch (error) {
   console.error("Database connection failed:", error);
   process.exit(1);
