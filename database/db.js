6877bf535eb48273c3b350eb0ffbaa09b4cc110b
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-24 03:08:18 +0500

    name2

--- database/db.js
+++ database/db.js
@@ -1,15 +1,16 @@
-import { Client } from "pg";
+import pkg from "pg";
+const { Client } = pkg;
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "./config/config.env" });
 
 const database = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER, // e.g., "postgres"
+  host: process.env.DB_HOST, // e.g., "db.cduapyvabihbfjtdzfln.supabase.co"
+  database: process.env.DB_NAME, // e.g., "postgres"
   password: process.env.DB_PASSWORD,
   port: 5432,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }, // required for Supabase
 });
 
 try {
