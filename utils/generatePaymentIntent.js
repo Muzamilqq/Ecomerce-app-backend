09f5e739ec0d2ac0f3263b531642641ea85a7eb9
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-25 06:00:23 +0500

    cokie change

--- utils/generatePaymentIntent.js
+++ utils/generatePaymentIntent.js
@@ -1,7 +1,9 @@
 import database from "../database/db.js";
 import Stripe from "stripe";
 
-const stripe = Stripe("PASTE_YOUR_STRIPE_SECRET_KEY");
+const stripe = Stripe(
+  "Psk_test_51Sz0X203AjB99ayMLhGe0MDx7AuMbdoekxiTWzPfQ491jKTrFhrvLDSOYsa0HeQryT5zTlH5AkLrBwP7FKIvGQgZ00YX7GhE4s",
+);
 
 export async function generatePaymentIntent(orderId, totalPrice) {
   try {
@@ -12,7 +14,7 @@ export async function generatePaymentIntent(orderId, totalPrice) {
 
     await database.query(
       "INSERT INTO payments (order_id, payment_type, payment_status, payment_intent_id) VALUES ($1, $2, $3, $4) RETURNING *",
-      [orderId, "Online", "Pending", paymentIntent.client_secret]
+      [orderId, "Online", "Pending", paymentIntent.client_secret],
     );
 
     return { success: true, clientSecret: paymentIntent.client_secret };
